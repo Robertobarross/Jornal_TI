89214bfec348d5d48d69c216341e995270a62f3a
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-02-28 22:51:11 -0300

    Create Form_cad.php

---
+++ Form_cad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Jornal_TI</title>
+    <link rel="stylesheet" type="text/css" href="css/formato.css">
+
+</head>
+<body>
+
+    <div id="Cadastro" align="center">
+
+    <h1 class="Titulo">Cadastre-se</h1>
+        
+    </div>
+
+</body>
+</html>
