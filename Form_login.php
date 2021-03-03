ed66967ab0437fd21973dc8bb951f5748885b1bb
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-02 22:55:06 -0300

    Criando o Form_login

---
+++ Form_login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Jornal_TI</title>
+    <link rel="stylesheet" type="text/css" href="css/formato.css">
+
+</head>
+<body>
+
+<div id="Login" align="center";
+
+     <h1 class="titulo">Login</h1>
+
+     <Form action="logar.php" method="POST" enctype="multipart/form-data">
+
+     <input type="email" name="email" class="camp_cad" placeholder="Email" required>
+     <input type="password" name="senha" class="camp_cad" placeholder="Senha" require.
+
+     <p><input type="submit" value="Logar" class="bt_cad">
+     <input type="reset" value="Limpar" class="bt_cad">
+
+     </form>
+
+</div>
+    
+</body>
+</html>
