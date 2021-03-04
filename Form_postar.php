427365c56381e8a9abaa065a387ba475f400af8a
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-04 20:37:36 -0300

    Finalizando o Form_postar.php

--- Form_postar.php
+++ Form_postar.php
@@ -8,9 +8,45 @@
 </head>
 <body>
 
+    <div id="Topo"><!-- Inicio da div top -->
+
+        <!-- Titulo da aplicação -->
+        <h1 class="Titulo_1">JTi<a class="Subtitulo">JORNAL TECNOLOGIA DA INFORMACAO</a></h1>
+            
+        <!-- Configuração com CSS do Topo, Titulo e Subtitulo -->   
+        <style type="text/css">
+        #Topo{width: 100%; height: 70px; background-color: brown; margin: 5px,5px; }
+        .Titulo_1{font-size: 60px; font-family: broadway; margin: 5px; margin-top: 3px; color: honeydew;}
+        .Subtitulo{font-size: 10px; font-family: Arial; float: right; margin: 8px; margin-top: 28px;}
+        </style>
+        
+    </div><!-- Fim da div topo -->
+
     <div id="Postagens" align="center">
 
-        <h1 class="titulo">Conteúdo</h1>
+        <h1 class="titulo">Públicar Conteúdo</h1>
+
+        <form action="Postar.php" method="$_POST" enctype="multipart/form-data">
+        
+        <br><input type="text" name="titulo" class="camp_cad" placeholder="Titulo da postagem" require></br>
+        <br><input type="file" name="foto" class="camp_cad"></br>
+        <br><textarea name="conteudo" class="texto" placeholder="Digite o conteúdo com no nomáximo 1.000 caracteres..." maxlength="1.000"></textarea></br>
+
+        <p><input type="submit" value="Publicar" class="bt_cad">
+        <input type="reset" value="Limpar" class="bt_cad">
+
+        </form>
+
+        <style type="text/css">/* Formato do titulo, campos e botões com css */
+        .titulo{font-size: 25px; font-family: broadway; padding: 2px;}
+        .camp_cad{width: 40%; margin: 2px; height: 15px; padding: 2px;}
+        .texto{width: 40%; margin: 2px; height: 250px; padding: 2px;}
+        .bt_cad{width:10%; padding: 2px; font-size: 15px; font-family: Arial; color: white; background-color: gray; }
+        .bt_cad:hover{background-color: brown;}
+        .Link{font-size: 15px; font-family: Arial; color:blue;}
+        </style>
+
+		<p><a href="index.php" class="link">&larr;Voltar para inicial</p>
 
     </div>
 
