ebdbc1232440393e567401d419a29194bffb6fda
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-06 22:30:53 -0300

    Finalizando e fazendo testes

--- Cont_post.php
+++ Cont_post.php
@@ -9,26 +9,30 @@
 <body>
 
 <div id="Conteudo">
-    <h1 class="titulo">Postagens</h1>
 
     <?php
 
     include "connect.php";
 
-    $titulo = $line['titulo'];
-    $foto = $line['foto'];
-    $texto = $line['texto'];
+    $titulo = $_GET['titulo']?? '';
+    $foto = $_GET['foto']?? '';
+    $texto = $_GET['texto']?? '';
 
-    ?>
+    ?> 
 
-<div class="postagens">
+<div id="postagens">
 
    <h1 class="titulo"><?php echo $titulo;?></h1>
-   <img src="foto"<?php echo $foto;?>class="imagem">
-   <p class="texto"><?php echo $texto;?></p>
+   <img src="postagens/<?php echo "post"."/".$foto;?>"class="Imagem">
+   <br><p class="conteudo"><?php echo $texto;?></p></br>
 
-
-    
+   <style type="text/css">
+   #postagens{width: 60%; height: auto; float: left; border: 1px solid lightgray; background-color: white; margin: 5px 1px;} 
+   .titulo{font-size: 15px; font-family:Times New Roman; padding: 2px; margin: 10px 5px;}
+   .Imagem{width: 75%; margin: 2px; height: 40%; padding: 2px;}
+   .conteudo{font-size: 13px; font-family: Times New Roman; margin: 5px;}
+   </style>
+   
 </div>
     
 </body>
