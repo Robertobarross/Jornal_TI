5975eb6a997f0386320f074c908b430529d778fe
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-10 22:44:19 -0300

    Alterações de rotina

--- Cont_post_II.php
+++ Cont_post_II.php
@@ -8,8 +8,6 @@
 </head>
 <body>
 
-<div id="Conteudo">
-
     <?php
 
     include "connect.php";
@@ -20,20 +18,17 @@
 
     ?> 
 
-<div id="postagens_II">
-
+   <h1 class="postagens_II"></h1>
    <h1 class="titulo"><?php echo $titulo;?></h1>
    <img src="postagens/<?php echo "post"."/".$foto;?>"class="Imagem">
    <br><p class="conteudo"><?php echo $texto;?></p></br>
 
    <style type="text/css">
-   #postagens_II{width: 28%; height: auto; float: right; border: 1px solid lightgray; background-color: white; margin: 5px 1px;}
+   .postagens_II{width: 30%; height: auto; float: right; border: 1px solid lightgray; background-color: white; margin: 5px 1px;}
    .titulo{font-size: 15px; font-family:Times New Roman; padding: 2px; margin: 10px 5px;}
-   .Imagem{width: 90%; margin: 2px; height: 40%; padding: 2px;}
+   .Imagem{width: 98%; margin: 2px; height: 40%; padding: 2px;}
    .conteudo{font-size: 13px; font-family: Times New Roman; margin: 5px;}
    </style>
-   
-</div>
     
 </body>
 </html>
