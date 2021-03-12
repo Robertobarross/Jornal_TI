5e7840cf74ed634a03b69239b15e7a9def0bfd01
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-11 23:08:16 -0300

    Corrigindo o caminho da postagem

---
+++ Postar_II.php
@@ -0,0 +1,22 @@
+<?php
+include 'connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jornal_TI</title>
+</head>
+<body>
+     
+     <div>
+     <?php
+     include "Cont_post_II.php";
+     ?>
+     </div>
+   
+</body>
+</html>
