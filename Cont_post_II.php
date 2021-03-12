7be41672bded1e278daa1e5ef23e6c41295e7a18
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-11 23:02:48 -0300

    Apagando itens desnecessários

--- Cont_post_II.php
+++ Cont_post_II.php
@@ -1,34 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jornal_TI</title>
-</head>
-<body>
+<?php  // O código a baixo é responsável por fazer a consulta no banco de dados
+include 'connect.php';
+$sql = mysqli_query($link, "SELECT * FROM tb_postar_II ORDER BY id_postar DESC");
+	while($line = mysqli_fetch_array($sql)){
+		$titulo = $line['titulo'];
+		$foto = $line['foto'];
+		$texto = $line['texto'];
+		$data = $line['dt'];
+		$id_postar = $line['id_postar'];
+		$hora = $line['hr'];
+?>
+<!-- Após feita a consulta, o código a baixo leva a postagens para página principal "Index.php "-->
+<h1 class="conteudo"></h1><!--Postagens dentro do conteúdo principal-->
+<h1 class="titulo"><?php echo $titulo;?></h1>
+<img src="postagens/<?php echo "post"."/".$foto;?>"class="Imagem">
+<p class="texto"><?php echo $texto;?></p>
+<span class="Data" class="hora"><?php echo date('m/d/y',strtotime ($data));echo" | ".$hora;?></span>
 
-    <?php
-
-    include "connect.php";
-
-    $titulo = $_GET['titulo']?? '';
-    $foto = $_GET['foto']?? '';
-    $texto = $_GET['texto']?? '';
-
-    ?> 
-
-   <h1 class="postagens_II"></h1>
-   <h1 class="titulo"><?php echo $titulo;?></h1>
-   <img src="postagens/<?php echo "post"."/".$foto;?>"class="Imagem">
-   <br><p class="conteudo"><?php echo $texto;?></p></br>
-
-   <style type="text/css">
-   .postagens_II{width: 30%; height: auto; float: right; border: 1px solid lightgray; background-color: white; margin: 5px 1px;}
+<style type="text/css"> /* Configuração dos campos com css */
    .titulo{font-size: 15px; font-family:Times New Roman; padding: 2px; margin: 10px 5px;}
-   .Imagem{width: 98%; margin: 2px; height: 40%; padding: 2px;}
-   .conteudo{font-size: 13px; font-family: Times New Roman; margin: 5px;}
-   </style>
-    
-</body>
-</html>
+   .Imagem{width: 100%; margin: 2px; height: 40%; padding: 2px;}
+   .texto{font-size: 13px; font-family: Times New Roman; margin: 5px;}
+   .Data{font-size: 10px; font-family: Times New Roman; margin: 5px;}
+</style>
+
+<?php  
+
+} 
+
+?>
