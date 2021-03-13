70feb33d1033245ac3e4c9696703d6b7fd4d69b8
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-12 22:09:04 -0300

    Ajustes finais

--- Form_postar.php
+++ Form_postar.php
@@ -11,13 +11,13 @@
     <div id="Topo"><!-- Inicio da div top -->
 
         <!-- Titulo da aplicação -->
-        <h1 class="Titulo_1">JTi<a class="Subtitulo">JORNAL TECNOLOGIA DA INFORMACAO</a></h1>
+        <h1 class="Titulo_1">JTi_notícias<a class="Subtitulo">JORNAL TECNOLOGIA DA INFORMACAO</a></h1>
             
         <!-- Configuração com CSS do Topo, Titulo e Subtitulo -->   
         <style type="text/css">
         #Topo{width: 100%; height: 70px; background-color: brown; margin: 5px,5px; }
-        .Titulo_1{font-size: 60px; font-family: broadway; margin: 5px; margin-top: 3px; color: honeydew;}
-        .Subtitulo{font-size: 10px; font-family: Arial; float: right; margin: 8px; margin-top: 28px;}
+        .Titulo_1{font-size: 60px; font-family: broadway; font-style:italic; margin: 5px; margin-top: 3px; color: honeydew;}
+        .Subtitulo{font-size: 10px; font-family: broadway; float: right; margin: 8px; margin-top: 28px;}
         </style>
         
     </div><!-- Fim da div topo -->
@@ -37,11 +37,13 @@
         <!-- Campos do formulário --> 
         <br><input type="text" name="titulo" class="camp_cad" placeholder="Titulo da postagem" require></br>
         <br><input type="file" name="foto" class="camp_cad"></br>
-        <textarea name="texto" class="conteudo" placeholder="Digite o conteúdo com no nomáximo 900 caracteres..." maxlength="900" require></textarea></br>
+        <textarea name="texto" class="conteudo" placeholder="Digite o conteúdo com no nomáximo 9999 caracteres..." maxlength="900" require></textarea></br>
 
         <!-- Botões -->
         <p><input type="submit" value="Publicar" class="bt_cad">
         <input type="reset" value="Limpar" class="bt_cad">
+
+        <p><a href="index.php" class="link">Voltar para página inicial</a> | <a href="Menu.php" class="link">Voltar ao Painel</a>
 
         </form>
 
@@ -50,17 +52,12 @@
         .titulo{font-size: 25px; font-family: broadway; padding:2px;}
         .camp_cad{width: 40%; margin: 2px; height: 15px; padding: 2px;}
         .conteudo{width: 40%; margin: 2px; height: 250px; padding: 2px;}
-        .bt_cad{width:10%; padding: 2px; font-size: 15px; font-family: Arial; color: white; background-color: gray; }
+        .bt_cad{width:10%; padding: 2px; font-size: 15px; font-family: Arial; color: white; background-color: gray; border-radius: 10px;}
         .bt_cad:hover{background-color: brown;}
-        .Link{width:40%;font-size: 15px; font-family: Arial; color:blue;}
+        .Link{font-size: 15px; font-family: Arial; color:blue;}
         </style>
 
-		<p><a href="index.php" class="link">&larr;Voltar para página inicial</p>
-        <p><a href="Menu.php" class="link">&larr;Voltar ao Painel</p>
-
-
     </div>
-
 
 </body>
 </html>
